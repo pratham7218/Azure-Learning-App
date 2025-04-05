27b48c59fd793e18b22bff247d7862210ee46a5a
Prathamesh Londhe <prathamesh.londhe@allata.in>
2025-04-05 12:12:37 +0530

    created deployment slot

---
+++ Azure-Learning-App/Pages/DeploymentSlot.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Azure_Learning_App.Pages
+{
+    public class DeploymentSlotModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
